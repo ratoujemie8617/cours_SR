fd0187e1a9a3a6ad6d6db65db39301c72e45cb16
ratoujemie8617 <jeremie.baudon.17@gmail.com>
2021-10-23 20:21:15 +0200

    modifications mineures

--- TP_26102021/TP_26102021-2.php
+++ TP_26102021/TP_26102021-2.php
@@ -1,11 +1,11 @@
 <?php
 // demande d'une saisie à l'utilisateur et attribution de la saisie à la variable $c
-echo "saisir un symbole ou une lettre ...";
+echo "saisir un caractère ...";
 $c = trim(fgets(STDIN));
 
-// demande d'une saisie à l'utilisateur et attribution de la saisie à la variable $n
-echo "saisir un nombre ...";
-$n = trim(fgets(STDIN));
+// demande d'une saisie à l'utilisateur et attribution de la saisie à la variable $long
+echo "saisir une longueur ...";
+$long = trim(fgets(STDIN));
 
 
 ?>
