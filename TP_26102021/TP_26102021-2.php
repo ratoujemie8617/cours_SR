6d06b385ac39a2770be496cdd325219ea7394609
ratoujemie8617 <jeremie.baudon.17@gmail.com>
2021-10-23 23:26:50 +0200

    mis à jour

--- TP_26102021/TP_26102021-2.php
+++
@@ -1,9 +0,0 @@
-<?php
-// demande d'une saisie à l'utilisateur et attribution de la saisie à la variable $c
-echo "saisir un caractère ...";
-$c = trim(fgets(STDIN));
-
-// demande d'une saisie à l'utilisateur et attribution de la saisie à la variable $long
-echo "saisir une longueur ...";
-$long = trim(fgets(STDIN));
-?>
